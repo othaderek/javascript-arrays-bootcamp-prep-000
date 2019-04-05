0fd439627968dc40799a3890d7d904a200617952
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-05 00:35:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  array = [element, ...array]
+  array = [...array, element]
   return array
 }
 

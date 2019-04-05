0ab0d0a5e127afa595a6d59d7f776865bd5a43f0
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-05 00:30:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   
   array = [...array, element]
-  console.log(array)
+  return array
   
 }
 

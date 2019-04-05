86400a9e94111cd0483a52c7e76bf55b2b550e94
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-05 01:34:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0)
+  array = array.slice(0)
   return array
   
 }

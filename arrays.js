2e827b511e8569da36a8c33207222f158921d194
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-05 00:29:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,24 +3,28 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   
   array = [...array, element]
+  console.log(array)
   
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   
   array = array.unshift(element)
-  
+  console.log(array)
 }
 
 function addElementToEndOfArray(array, element) {
   array = [element, ...array]
+  console.log(array)
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array = array.push(element)
+  console.log(array)
 }
 
 function accessElementInArray(array, index) {
   console.log(array[index])
+  
 }
 
